7b990fab1fc79a3798e320879841b51e42791d71
lcadena <36576978+lcadena@users.noreply.github.com>
2018-02-22 20:07:45 +0100

    Segundo Commit

---
+++ src/Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    public String Nombre;
+    public String Password;
+    public int Nivel;
+    public int Ataque;
+    public int Defensa;
+    public int Resistencia;
+
+}
